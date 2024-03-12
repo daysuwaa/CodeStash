a435e43a9d07df9f59d6faa6e039f79c387a3205
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-03-12 15:03:47 +0100

    added new components to the app.js file and gave them their respective routes

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "/src/components/Home.jsx";
+import Signin from "./components/Signin";
+import Login from "./components/Login";
+import Forgotpassword from "./components/Forgottenpassword";
+import Stashed from "./components/Stashed";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Signin />} />
+          <Route path="/fpass" element={<Forgotpassword />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/stashed" element={<Stashed />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
