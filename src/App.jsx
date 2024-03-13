d2eb20a2beb91a6c70a27b55001397e4e86503d3
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-03-13 10:03:42 +0100

    removed all the routes and added it to the routes component and imported it

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "/src/components/Home.jsx";
-import Signin from "./components/Signin";
-import Login from "./components/Login";
-import Forgotpassword from "./components/Forgottenpassword";
-import Stashed from "./components/Stashed";
+import AppRoutes from "./Routes";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Signin />} />
-          <Route path="/fpass" element={<Forgotpassword />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/stashed" element={<Stashed />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
+      <AppRoutes />
     </>
   );
 }
