ade8d919d716233329ddeecabd466a2f35c2286e
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-03-16 19:35:14 +0100

    commit: changed the icon of the backbutton component

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Link } from "react-router-dom";
-import { SlArrowLeft } from "react-icons/sl";
+import { IoIosArrowRoundBack } from "react-icons/io";
 import PropTypes from "prop-types";
 
 const BackButton = ({ destination = "/" }) => {
   return (
     <div>
-      <Link to={destination} className=" px-4 py-1 rounded-lg w-fit">
-        <SlArrowLeft className="text-teal-500" />
+      <Link to={destination} className=" py-1 rounded-lg w-fit">
+        <IoIosArrowRoundBack className="text-blue-900 text-5xl cursor-pointer hover:text-blue-300" />
       </Link>
     </div>
   );
