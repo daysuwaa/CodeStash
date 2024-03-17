743557caaf498c2ce2e11a2cc0e2e51a52f822ae
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-03-17 20:09:31 +0100

    commit:gave the nav bar  a mt-[5rem]

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="mt-9 flex justify-center">
+    <div className="mt-[5rem] flex justify-center">
       <h1 className="font-fontfamily3 md:text-5xl text-4xl font-bold">
         <span className="bg-clip-text text-transparent bg-gradient-to-r from-white to-blue-700">
           CodeStash
