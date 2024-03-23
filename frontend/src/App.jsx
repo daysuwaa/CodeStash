1cadff774f4d0e69b19757072a166bc833374556
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-03-23 16:43:38 +0100

    commit:fixed route problem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,44 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useAuthContext } from "./pages/AuthPages/useAuthContext";
-import Home from "./pages/Home";
-import AddCode from "./pages/AddCode";
-import Stash from "./pages/Stash";
-import EditCode from "./pages/EditCode";
-import DeleteCode from "./pages/DeleteCode";
-import Signup from "./pages/AuthPages/Signin";
-import Login from "./pages/AuthPages/Login";
-import Navbar from "./components/Navbar";
-import Getstarted from "./pages/Getstarted";
-// import Forgotpassword from "./pages/AuthPages/ForgottenPassword";
-const App = () => {
-  const { user } = useAuthContext();
-  return (
-    <div>
-      <Route>
-        <Route exact path="/getstarted" element={<Getstarted />} />
-      </Route>
-      <Navbar />
-      <Routes>
-        {/* <Route exact path="/getstarted" element={<Getstarted />} /> */}
-        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
-        <Route path="/codes/add" element={<AddCode />} />
-        <Route path="/codes/details/:id" element={<Stash />} />
-        <Route path="/codes/edit/:id" element={<EditCode />} />
-        <Route path="/codes/delete/:id" element={<DeleteCode />} />
-        <Route
-          path="/signup"
-          element={!user ? <Signup /> : <Navigate to="/" />}
-        />
-        <Route
-          path="/login"
-          element={!user ? <Login /> : <Navigate to="/" />}
-        />
-      </Routes>
-    </div>
-  );
-};
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import { useAuthContext } from "./pages/AuthPages/useAuthContext";
+// import Home from "./pages/Home";
+// import AddCode from "./pages/AddCode";
+// import Stash from "./pages/Stash";
+// import EditCode from "./pages/EditCode";
+// import DeleteCode from "./pages/DeleteCode";
+// import Signup from "./pages/AuthPages/Signin";
+// import Login from "./pages/AuthPages/Login";
+// import Navbar from "./components/Navbar";
+// import Getstarted from "./pages/Getstarted";
+// // import Forgotpassword from "./pages/AuthPages/ForgottenPassword";
+// const App = () => {
+//   const { user } = useAuthContext();
+//   return (
+//     <div>
+//       <Route>
+//         <Route exact path="/getstarted" element={<Getstarted />} />
+//       </Route>
+//       <Navbar />
+//       <Routes>
+//         {/* <Route exact path="/getstarted" element={<Getstarted />} /> */}
+//         <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+//         <Route path="/codes/add" element={<AddCode />} />
+//         <Route path="/codes/details/:id" element={<Stash />} />
+//         <Route path="/codes/edit/:id" element={<EditCode />} />
+//         <Route path="/codes/delete/:id" element={<DeleteCode />} />
+//         <Route
+//           path="/signup"
+//           element={!user ? <Signup /> : <Navigate to="/" />}
+//         />
+//         <Route
+//           path="/login"
+//           element={!user ? <Login /> : <Navigate to="/" />}
+//         />
+//       </Routes>
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
 
 // import { Routes, Route, Navigate } from "react-router-dom";
 // import { useAuthContext } from "./pages/AuthPages/useAuthContext";
@@ -66,8 +66,7 @@ export default App;
 //           element={
 //             user ? (
 //               <>
-//                 <Navbar />
-//                 {/* <Home /> */}
+//                 <Navbar /> <Home />
 //               </>
 //             ) : (
 //               <Navigate to="/login" />
@@ -92,6 +91,7 @@ export default App;
 // };
 
 // export default App;
+
 // import { Routes, Route, Navigate } from "react-router-dom";
 // import { useAuthContext } from "./pages/AuthPages/useAuthContext";
 // import Home from "./pages/Home";
@@ -101,7 +101,7 @@ export default App;
 // import DeleteCode from "./pages/DeleteCode";
 // import Signup from "./pages/AuthPages/Signin";
 // import Login from "./pages/AuthPages/Login";
-// import Navbar from "./components/Navbar";
+// // import Navbar from "./components/Navbar";
 // import Getstarted from "./pages/Getstarted";
 
 // const App = () => {
@@ -109,15 +109,21 @@ export default App;
 
 //   return (
 //     <div>
-//       <Navbar />
+//       {/* <Navbar /> */}
 //       <Routes>
 //         <Route exact path="/getstarted" element={<Getstarted />} />
 //         <Route path="/codes/add" element={<AddCode />} />
 //         <Route path="/codes/details/:id" element={<Stash />} />
 //         <Route path="/codes/edit/:id" element={<EditCode />} />
 //         <Route path="/codes/delete/:id" element={<DeleteCode />} />
-//         <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />}/>
-//         <Route path="/login"element={!user ? <Login /> : <Navigate to="/" />} />
+//         <Route
+//           path="/signup"
+//           element={!user ? <Signup /> : <Navigate to="/" />}
+//         />
+//         <Route
+//           path="/login"
+//           element={!user ? <Login /> : <Navigate to="/" />}
+//         />
 //         <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
 //       </Routes>
 //     </div>
@@ -165,3 +171,44 @@ export default App;
 // };
 
 // export default App;
+
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./pages/AuthPages/useAuthContext";
+import Home from "./pages/Home";
+import AddCode from "./pages/AddCode";
+import Stash from "./pages/Stash";
+import EditCode from "./pages/EditCode";
+import DeleteCode from "./pages/DeleteCode";
+import Signup from "./pages/AuthPages/Signin";
+import Login from "./pages/AuthPages/Login";
+import Getstarted from "./pages/Getstarted";
+
+const App = () => {
+  const { user } = useAuthContext();
+
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/getstarted" element={<Getstarted />} />
+        <Route path="/codes/add" element={<AddCode />} />
+        <Route path="/codes/details/:id" element={<Stash />} />
+        <Route path="/codes/edit/:id" element={<EditCode />} />
+        <Route path="/codes/delete/:id" element={<DeleteCode />} />
+        <Route
+          path="/signup"
+          element={!user ? <Signup /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/"
+          element={user ? <Home /> : <Navigate to="/getstarted" />}
+        />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
